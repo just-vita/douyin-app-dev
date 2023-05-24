cb79cf19fe90a6f8be7fe2ee1b4fd88cab3ef91f
just-vita <1254581982@qq.com>
2023-05-24 12:57:17 +0800

    完善配置文件

--- book-api/src/main/java/top/vita/Application.java
+++ book-api/src/main/java/top/vita/Application.java
@@ -1,5 +1,6 @@
 package top.vita;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2023/5/23 23:36
  */
 @SpringBootApplication
+@MapperScan(basePackages = "top.vita.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
