615838e4dc4a00e8f5f2cb6161bfc183e3f101bf
just-vita <1254581982@qq.com>
2023-05-24 16:03:16 +0800

    VO、BO、SID全局Id生成工具及其他工具类

--- book-api/src/main/java/top/vita/Application.java
+++ book-api/src/main/java/top/vita/Application.java
@@ -3,6 +3,7 @@ package top.vita;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Author vita
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan(basePackages = "top.vita.mapper")
+@ComponentScan(basePackages = {"top.vita", "org.n3r.idworker"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ book-common/src/main/java/top/vita/utils/MyInfo.java
@@ -0,0 +1,9 @@
+package top.vita.utils;
+
+public class MyInfo {
+
+    public static String getMobile() {
+        return "";
+    }
+
+}
